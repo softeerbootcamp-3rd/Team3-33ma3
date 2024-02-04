a8537a8800b7ab243774f62bfc3d6ab86aff7d8c
cmw9957 <cmw9957@gmail.com>
2024-02-04 22:40:04 +0900

    [FEAT]: styles 공통 변수 추가

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: "Pretendard";
+        font-style: normal;
+        line-height: normal;
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,64 @@
+const DefaultTheme = {
+  fontSize: {
+    title: "40px",
+    large: "35px",
+    medium: "28",
+    regular: "20px",
+    small: "15px",
+  },
+  colors: {
+    text_strong: "#14142B",
+    text_bold: "#424B66",
+    text_default: "#6E7191",
+    text_weak: "#A0A3BD",
+    text_white_default: "#FEFEFE",
+    text_white_weak: "#F7F7FC",
+    text_brand: "#0E1D4F",
+    text_red: "#FF3B30",
+
+    surface_default: "#FEFEFE",
+    surface_alt: "#F7F7FC",
+    surface_brand: "#0E1D4F",
+    surface_black: "#000000",
+    surface_weak: "#DCDCDC",
+    surface_white_weak: "#EEEEEE",
+    surface_opacity_50: {
+      color: "#0E1D4F",
+      opacity: "50%",
+    },
+    surface_opacity_75: {
+      color: "#0E1D4F",
+      opacity: "75%",
+    },
+    border_default: "#F2F2F6",
+    border_strong: "#A5A5A5",
+  },
+  rediuses: {
+    radius_s: "10px",
+    radius_m: "16px",
+    radius_l: "50%",
+  },
+  dropShadow: {
+    normal: {
+      border_radius: "8px",
+      background: "#FEFEFE",
+      box_shadow: "0px 1px 4px 0px rgba(110, 128, 145, 0.24)",
+    },
+    up: {
+      border_radius: "8px",
+      background: "#FEFEFE",
+      box_shadow:
+        "0px 2px 8px 0px rgba(110, 128, 145, 0.16), 0px 2px 8px 0px rgba(110, 128, 145, 0.16)",
+      backdrop_filter: "blur(2px)",
+    },
+    floating: {
+      border_radius: "8px",
+      background: "#FEFEFE",
+      box_shadow: "0px 1px 4px 0px rgba(110, 128, 145, 0.24)",
+      box_shadow:
+        "0px 16px 16px 0px rgba(110, 128, 145, 0.24), 0px 0px 4px 0px rgba(110, 128, 145, 0.08)",
+    },
+  },
+};
+
+export default DefaultTheme;
