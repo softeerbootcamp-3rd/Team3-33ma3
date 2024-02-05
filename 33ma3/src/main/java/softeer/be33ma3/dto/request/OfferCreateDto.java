9cdd014c5d15fdd253e20501dc2df0b8614dcd6c
jbonnie <bonnie8623@gmail.com>
2024-02-05 16:19:36 +0900

    [FEAT]: 새로 생성할 댓글 내용을 받아올 dto 클래스 구현
    - OfferCreateDto 구현
    - OfferCreateDto -> Offer entity로 변환하는 toEntity 메소드 구현

---
+++ 33ma3/src/main/java/softeer/be33ma3/dto/request/OfferCreateDto.java
@@ -0,0 +1,21 @@
+package softeer.be33ma3.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import softeer.be33ma3.domain.Center;
+import softeer.be33ma3.domain.Offer;
+import softeer.be33ma3.domain.Post;
+
+@Getter
+public class OfferCreateDto {
+    private int price;
+    private String contents;
+
+    public Offer toEntity(Post post, Center center) {
+        return Offer.builder()
+                .price(price)
+                .contents(contents)
+                .post(post)
+                .center(center).build();
+    }
+}
