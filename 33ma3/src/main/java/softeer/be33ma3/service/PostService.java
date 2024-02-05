d2ea2ecc793780ae42d658f8e1110af612f65ab9
jbonnie <bonnie8623@gmail.com>
2024-02-05 12:12:27 +0900

    [FEAT]: 게시글의 평균 견적 제시 가격 반환 메소드 구현
    - PostService에 priceAvgOfPost 메소드 구현
    - 해당 게시글에 속해있는 견적 제시 댓글의 제시 가격을 평균내어 반환

--- 33ma3/src/main/java/softeer/be33ma3/service/PostService.java
+++ 33ma3/src/main/java/softeer/be33ma3/service/PostService.java
@@ -1,4 +1,31 @@
 package softeer.be33ma3.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import softeer.be33ma3.domain.Offer;
+import softeer.be33ma3.repository.OfferRepository;
+import softeer.be33ma3.repository.PostRepository;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private PostRepository postRepository;
+    private OfferRepository offerRepository;
+
+    // 해당 게시글의 평균 견적 제시 가격 반환
+    private double priceAvgOfPost(Long postId) {
+        // 해당 게시글의 견적 모두 가져오기
+        List<Offer> offerList = offerRepository.findAllByPostId(postId);
+
+        // 제시 가격의 합계, 개수 구하기
+        IntSummaryStatistics stats = offerList.stream()
+                .collect(Collectors.summarizingInt(Offer::getPrice));
+
+        return stats.getAverage();
+    }
 }
