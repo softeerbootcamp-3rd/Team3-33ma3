31b0ab6ef97739b67ebd2e88e8a9ff6b8de0d92a
cmw9957 <cmw9957@gmail.com>
2024-02-05 17:12:53 +0900

    [STYLE]: ChipButton border에서 box-shadow로 변경

--- src/components/ChipButton.jsx
+++ src/components/ChipButton.jsx
@@ -7,16 +7,13 @@ const Button = styled.button`
   font-weight: 500;
 
   padding: 5px 15px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   border-radius: ${(props) => props.theme.radiuses.radius_m};
-  border: 1px solid ${(props) => props.theme.colors.border_strong};
+  box-shadow: 0px 0px 0px 1.5px ${(props) => props.theme.colors.border_strong};
   background: ${(props) => props.theme.colors.surface_default};
 
   &:hover,
   &.active {
-    border: 1.5px solid ${(props) => props.theme.colors.surface_black};
+    box-shadow: 0px 0px 0px 1.5px ${(props) => props.theme.colors.surface_black};
     background: ${(props) => props.theme.colors.surface_default};
     color: ${(props) => props.theme.colors.text_strong};
   }
