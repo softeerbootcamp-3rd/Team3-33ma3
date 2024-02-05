3ffb43d9f34f4ca9c45cacc6cead90df491264f7
jbonnie <bonnie8623@gmail.com>
2024-02-05 10:41:54 +0900

    [FEAT]: PostRepository 수정
    - JpaRepository extends 하는 것으로 수정

--- 33ma3/src/main/java/softeer/be33ma3/repository/PostRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package softeer.be33ma3.repository;
 
-public interface PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import softeer.be33ma3.domain.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
