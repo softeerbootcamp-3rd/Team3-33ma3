22ca0ae707bc5679078c37de0842636245863655
jbonnie <bonnie8623@gmail.com>
2024-02-06 16:08:02 +0900

    [FEAT]: 게시글 세부사항에 이미지 리스트 필드 추가
    - 해당 게시물에 속해있는 이미지의 링크를 리스트 형태로 저장

--- 33ma3/src/main/java/softeer/be33ma3/dto/response/PostDetailDto.java
+++ 33ma3/src/main/java/softeer/be33ma3/dto/response/PostDetailDto.java
@@ -1,6 +1,7 @@
 package softeer.be33ma3.dto.response;
 
 import lombok.Builder;
+import softeer.be33ma3.domain.Image;
 import softeer.be33ma3.domain.Post;
 
 import java.time.LocalDateTime;
@@ -19,11 +20,13 @@ public class PostDetailDto {
     private String contents;
     private List<String> repairList;
     private List<String> tuneUpList;
+    private List<String> imageList;
 
     // Post Entity -> PostDetailDto 변환
     public static PostDetailDto fromEntity(Post post) {
         List<String> repairList = stringCommaParsing(post.getRepairService());
         List<String> tuneUpList = stringCommaParsing(post.getTuneUpService());
+        List<String> imageList = post.getImages().stream().map(Image::getLink).toList();
         int betweenTime = (int)ChronoUnit.DAYS.between(LocalDateTime.now(), post.getCreateTime());
         return PostDetailDto.builder()
                 .postId(post.getPostId())
@@ -34,7 +37,8 @@ public class PostDetailDto {
                 .regionName(post.getRegion().getRegionName())
                 .contents(post.getContents())
                 .repairList(repairList)
-                .tuneUpList(tuneUpList).build();
+                .tuneUpList(tuneUpList)
+                .imageList(imageList).build();
     }
 
     // 구분자 콤마로 문자열 파싱 후 각각의 토큰에서 공백 제거 후 리스트 반환
