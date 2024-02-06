999abc46547ad582c29d3e809072660d895d3d2b
jbonnie <bonnie8623@gmail.com>
2024-02-06 15:23:46 +0900

    [FEAT]: jpa query method 추가
    - 게시글 세부사항 조회 기능에 사용할 쿼리 메소드 구현

--- 33ma3/src/main/java/softeer/be33ma3/repository/CenterRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/CenterRepository.java
@@ -3,5 +3,9 @@ package softeer.be33ma3.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import softeer.be33ma3.domain.Center;
 
+import java.util.Optional;
+
 public interface CenterRepository extends JpaRepository<Center, Long> {
+
+    Optional<Center> findByMember_MemberId(Long memberId);
 }
