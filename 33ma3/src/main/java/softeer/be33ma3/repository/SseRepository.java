7535670d3a0fae5f0253726b3d0f934d54795ddc
jbonnie <bonnie8623@gmail.com>
2024-02-06 21:38:52 +0900

    [FEAT]: SSE 연결 요청 기능 구현
    - 유저가 SSE 연결을 요청하면 새로운 SseEmitter 객체를 생성, Map 타입 저장소에 저장

--- 33ma3/src/main/java/softeer/be33ma3/repository/SseRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/SseRepository.java
@@ -1,12 +1,19 @@
 package softeer.be33ma3.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class SseRepository {
 
     private final Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+
+    public SseEmitter save(Long memberId, SseEmitter sseEmitter) {
+        sseEmitters.put(memberId, sseEmitter);
+        return sseEmitter;
+    }
 
 }
