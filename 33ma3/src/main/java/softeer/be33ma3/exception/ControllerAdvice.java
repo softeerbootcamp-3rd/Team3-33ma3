863833e28d2a66b7bff1947d1b5a54529d2e03ef
jbonnie <bonnie8623@gmail.com>
2024-02-06 10:19:06 +0900

    [Feat]: @Valid 검증 시 발생하는 예외 처리 추가
    - MethodArgumentNotValidException 발생시 bad request 응답 반환

--- 33ma3/src/main/java/softeer/be33ma3/exception/ControllerAdvice.java
+++ 33ma3/src/main/java/softeer/be33ma3/exception/ControllerAdvice.java
@@ -1,12 +1,18 @@
 package softeer.be33ma3.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import softeer.be33ma3.response.SingleResponse;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValid(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(SingleResponse.error(e.getFieldError().getDefaultMessage()));
+    }
     @ExceptionHandler({IllegalArgumentException.class, ArithmeticException.class})
     public ResponseEntity<?> badRequest(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(SingleResponse.error(e.getMessage()));
