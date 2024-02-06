ead5af5cc1b4e2cba83e84c4897ca4573adfaecd
Yang-soeun <soeun8636@naver.com>
2024-02-06 12:40:50 +0900

    [STYLE]: findByRegionName 메소드 파라미터 이름 변경

--- 33ma3/src/main/java/softeer/be33ma3/repository/RegionRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/RegionRepository.java
@@ -5,5 +5,5 @@ import softeer.be33ma3.domain.Region;
 import java.util.Optional;
 
 public interface RegionRepository extends JpaRepository<Region, Long>{
-    Optional<Region> findByRegionName(String region);
+    Optional<Region> findByRegionName(String regionName);
 }
