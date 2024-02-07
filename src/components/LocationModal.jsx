72a0f745c67b5a44ae78e06ba3b5ec2ec268b1c1
cmw9957 <cmw9957@gmail.com>
2024-02-07 15:53:29 +0900

    [STYLE]: Modal 스타일링

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -2,6 +2,7 @@ import { forwardRef } from "react";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
 import ViewCurrentLocation from "./ViewCurrentLocation";
+import OptionTitle from "./title/OptionTitle";
 
 const Dialog = styled.dialog`
   display: inline-flex;
@@ -10,13 +11,37 @@ const Dialog = styled.dialog`
   align-items: center;
   gap: 30px;
 
-  ${(props) => props.theme.dropShadow.floating}
+  border-radius: ${(props) => props.theme.radiuses.radius_m};
+  box-shadow: ${(props) => props.theme.boxShadow.floating};
+`;
+
+const Title = styled.p`
+  color: ${(props) => props.theme.colors.surface_black};
+  font-size: ${(props) => props.theme.fontSize.medium};
+  font-weight: 700;
+`;
+
+const CloseButton = styled.button`
+  width: 35px;
+  height: 35px;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  width: 100%;
+  justify-content: space-between;
 `;
 
 const LocationModal = forwardRef(function LocationModal(props, ref) {
   return createPortal(
     <>
-      <Dialog ref={ref} className="location-modal">
+      <Dialog ref={ref} className="location-modal" open>
+        <TopContainer>
+          <Title>위치선택</Title>
+          <CloseButton>X</CloseButton>
+        </TopContainer>
         <ViewCurrentLocation />
       </Dialog>
     </>,
