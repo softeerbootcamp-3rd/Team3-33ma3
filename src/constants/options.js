8d3682d4df06ed881edd2bf53d6fc05d8c440fd2
chjih <81521128+chjih@users.noreply.github.com>
2024-02-10 02:44:35 +0900

    [FEAT]: 게시물 작성 시 option들 상수로 분리

---
+++ src/constants/options.js
@@ -0,0 +1,23 @@
+export const REPAIR_SERVICE_OPTIONS = [
+  "판금",
+  "흠집 제거",
+  "덴트",
+  "부품 교체",
+  "광택/코팅",
+  "기타",
+];
+
+export const TUNEUP_SERVICE_OPTIONS = [
+  "엔진 오일",
+  "타이어 교체",
+  "도어",
+  "기타",
+];
+
+export const CAR_CATEGORY_OPTIONS = [
+  "수소/전기차",
+  "승용차",
+  "SUV",
+  "트럭",
+  "버스",
+];
