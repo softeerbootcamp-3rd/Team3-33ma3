b8a6f11869b0a065deb4b03af981a23f86a8bc0b
Yang-soeun <soeun8636@naver.com>
2024-02-11 01:27:19 +0900

    [FEAT]: JwtTokenException -> RuntimeException으로 수정

--- 33ma3/src/main/java/softeer/be33ma3/exception/JwtTokenException.java
+++ 33ma3/src/main/java/softeer/be33ma3/exception/JwtTokenException.java
@@ -1,6 +1,6 @@
 package softeer.be33ma3.exception;
 
-public class JwtTokenException extends IllegalArgumentException {
+public class JwtTokenException extends RuntimeException {
     public JwtTokenException(String message) {
         super(message);
     }
