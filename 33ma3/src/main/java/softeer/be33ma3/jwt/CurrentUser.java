6987aca2f8dc12a738aedeb385ef82bb1483ec56
Yang-soeun <soeun8636@naver.com>
2024-02-11 01:32:50 +0900

    [FEAT]: 주석 오타 수정

--- 33ma3/src/main/java/softeer/be33ma3/jwt/CurrentUser.java
+++ 33ma3/src/main/java/softeer/be33ma3/jwt/CurrentUser.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)  //파라미터에서 사용한다는 의미
-@Retention(RetentionPolicy.RUNTIME) //리플렉션 등을 활용할 수 있도록 런타임까지 애노테이 션 정보가 남아도록 설정
+@Retention(RetentionPolicy.RUNTIME) //리플렉션 등을 활용할 수 있도록 런타임까지 애노테이션 정보가 남아도록 설정
 public @interface CurrentUser {
 }
