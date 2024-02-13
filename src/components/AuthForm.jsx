40bdd7d0e0e13dbe5f255a07028e955fc7be4b7c
cmw9957 <cmw9957@gmail.com>
2024-02-13 17:22:23 +0900

    [FEAT]: 회원가입시 center 양식 추가

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -12,23 +12,67 @@ function AuthForm() {
 
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
+  const userType = searchParams.get("type");
   const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post">
-        <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
-        </p>
-        <p>
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required />
-        </p>
-        <button disabled={isSubmitting}>
-          {isSubmitting ? "전송중..." : "저장하기"}
-        </button>
+        <h1>{isLogin ? "Log in" : `Create a new ${userType}`}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+        {!isLogin && !userType && (
+          <>
+            <Link to="?mode=signUp&type=client">Client</Link>
+            <Link to="?mode=signUp&type=center">Center</Link>
+          </>
+        )}
+        {(userType || isLogin) && (
+          <>
+            <p>
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" name="email" required />
+            </p>
+            <p>
+              <label htmlFor="password">Password</label>
+              <input id="password" type="password" name="password" required />
+            </p>
+            {userType === "center" && (
+              <>
+                <p>
+                  <label htmlFor="centerName">Center name</label>
+                  <input
+                    id="centerName"
+                    type="text"
+                    name="centerName"
+                    required
+                  />
+                </p>
+                <p>
+                  <label htmlFor="address">Address</label>
+                  <input id="address" type="text" name="address" required />
+                </p>
+              </>
+            )}
+            <div>
+              <Link to={`?mode=${isLogin ? "signUp" : "login"}`}>
+                {isLogin ? "Create new user" : "Login"}
+              </Link>
+              <button disabled={isSubmitting}>
+                {isSubmitting ? "전송중..." : "저장하기"}
+              </button>
+            </div>
+          </>
+        )}
       </Form>
     </>
   );
 }
+
+export default AuthForm;
