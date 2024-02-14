6620c35bf06d9dc3f2943727c84137afcfacb9d9
cmw9957 <cmw9957@gmail.com>
2024-02-14 10:06:36 +0900

    [FEAT]: center 회원가입시 주소 정보 입력 코드 추가

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Form,
   Link,
@@ -6,7 +7,34 @@ import {
   useNavigation,
 } from "react-router-dom";
 
+function searchAddressToCoordinate(address) {
+  // Promise 객체를 반환합니다.
+  return new Promise((resolve, reject) => {
+    naver.maps.Service.geocode(
+      {
+        query: address ? address : "DEFAULT",
+      },
+      function (status, response) {
+        if (status === naver.maps.Service.Status.ERROR) {
+          // 오류 상태일 경우 reject를 호출하여 Promise를 reject 상태로 만듭니다.
+          return reject("Something went Wrong!");
+        }
+
+        if (response.v2.meta.totalCount === 0) {
+          // 주소 검색 결과가 없을 경우
+          return resolve(null);
+        }
+
+        const item = response.v2.addresses[0]; // 찾은 주소 정보
+        // 성공적으로 주소 정보를 찾았을 경우 resolve를 호출하여 Promise를 resolve 상태로 만듭니다.
+        resolve(item);
+      }
+    );
+  });
+}
+
 function AuthForm() {
+  const [centerInformation, setCenterInformation] = useState();
   const data = useActionData();
   const navigation = useNavigation();
 
@@ -14,6 +42,19 @@ function AuthForm() {
   const isLogin = searchParams.get("mode") === "login";
   const userType = searchParams.get("type");
   const isSubmitting = navigation.state === "submitting";
+
+  function handleInputCenterName(e) {
+    searchAddressToCoordinate(e.target.value)
+      .then((res) => {
+        if (res !== null) {
+          setCenterInformation();
+        }
+        console.log(res); // 성공적으로 주소 정보를 받아왔을 때의 처리
+      })
+      .catch((error) => {
+        console.error(error); // 오류 발생 시 처리
+      });
+  }
 
   return (
     <>
@@ -56,7 +97,13 @@ function AuthForm() {
                 </p>
                 <p>
                   <label htmlFor="address">Address</label>
-                  <input id="address" type="text" name="address" required />
+                  <input
+                    id="address"
+                    type="text"
+                    name="address"
+                    onChange={handleInputCenterName}
+                    required
+                  />
                 </p>
               </>
             )}
