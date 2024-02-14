635b952081db6685a10048c7c8d0734e39257125
chjih <81521128+chjih@users.noreply.github.com>
2024-02-14 16:23:15 +0900

    [FEAT]: Timer 구현

---
+++ src/components/Timer.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+function Timer({ remainTime }) {
+  const [leftTime, setLeftTime] = useState(remainTime);
+  const hours = Math.floor(leftTime / 60 / 60);
+  const minutes = Math.floor(leftTime / 60) % 60;
+  const seconds = leftTime % 60;
+  console.log(leftTime);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setLeftTime(leftTime - 1);
+    }, 1000);
+
+    if (leftTime <= 0) {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [leftTime]);
+
+  function formTime(time) {
+    if (time < 10) {
+      return "0" + time;
+    }
+    return time;
+  }
+
+  return (
+    <>
+      {formTime(hours)}:{formTime(minutes)}:{formTime(seconds)}
+    </>
+  );
+}
+
+export default Timer;
