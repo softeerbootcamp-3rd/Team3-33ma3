d32050afee6b21db4ab32f04fc42c96fce25752b
chjih <81521128+chjih@users.noreply.github.com>
2024-02-14 10:42:07 +0900

    [FEAT]: 경매 현황 웹소켓 연결 구현

--- src/pages/PostPage/components/AuctionResult.jsx
+++ src/pages/PostPage/components/AuctionResult.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import BidPaddle from "../../../components/BidPaddle";
-import Comment from "../../../components/Comment";
-import BidPaddleList from "./BidPaddleList";
+import AuctionPrice from "../../../components/post/AuctionPrice";
+import Comment from "../../../components/post/Comment";
 import OptionType from "../../../components/post/OptionType";
+import OfferList from "./OfferList";
 
 const AwardedBid = styled.div`
   display: flex;
@@ -11,14 +11,14 @@ const AwardedBid = styled.div`
   gap: 30px;
 `;
 
-function AuctionResult({ bidList }) {
+function AuctionResult({ offerList }) {
   return (
     <OptionType title={"경매 결과"}>
       <AwardedBid>
-        <BidPaddle centerName={"강남"} price={"12"} isActive />
+        <AuctionPrice centerName={"강남"} price={"12"} isActive />
         <Comment centerName={"강남"} contents={"경매 성공자!"} disabled />
       </AwardedBid>
-      <BidPaddleList bidList={bidList} disabled />
+      <OfferList offerList={offerList} disabled />
     </OptionType>
   );
 }
