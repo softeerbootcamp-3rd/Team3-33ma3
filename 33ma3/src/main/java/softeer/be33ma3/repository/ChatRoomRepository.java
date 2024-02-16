d04e2bc52c222a3d2019f0b01273143e0fc2ecd1
jbonnie <123135030+jbonnie@users.noreply.github.com>
2024-02-16 13:32:27 +0900

    [FIX]: jpa query method naming 수정

--- 33ma3/src/main/java/softeer/be33ma3/repository/ChatRoomRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/ChatRoomRepository.java
@@ -6,7 +6,7 @@ import softeer.be33ma3.domain.ChatRoom;
 import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    List<ChatRoom> findByCenter_CenterId(Long memberId);
+    List<ChatRoom> findByCenter_MemberId(Long memberId);
 
-    List<ChatRoom> findByClient_ClientId(Long memberId);
+    List<ChatRoom> findByClient_MemberId(Long memberId);
 }
