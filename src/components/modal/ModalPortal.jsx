3cfd97d19b959dba77d11968dbbbd8c22e67647f
chjih <81521128+chjih@users.noreply.github.com>
2024-02-17 14:37:53 +0900

    [FEAT]: ModalPortal 컴포넌트 생성

---
+++ src/components/modal/ModalPortal.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
+import CloseImg from "../../assets/close.svg";
+
+const Modal = styled.div`
+  position: absolute;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(134, 134, 134, 0.53);
+`;
+
+const ModalBody = styled.div`
+  padding: 30px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: ${({ width }) => width};
+  border-radius: ${(props) => props.theme.radiuses.radius_m};
+  box-shadow: ${(props) => props.theme.boxShadow.floating};
+  background-color: ${({ theme }) => theme.colors.surface_default};
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Title = styled.p`
+  color: ${(props) => props.theme.colors.text_strong};
+  font-size: ${(props) => props.theme.fontSize.medium};
+  font-weight: 700;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+`;
+
+const CloseButton = styled.button`
+  width: 35px;
+  height: 35px;
+`;
+
+const ModalContent = styled.div`
+  width: 100%;
+`;
+
+function ModalPortal({ width, handleClose, children }) {
+  return createPortal(
+    <Modal>
+      <ModalBody width={width}>
+        <ModalHeader>
+          <Title>dkdkdkdk</Title>
+          <CloseButton onClick={handleClose}>
+            <img src={CloseImg} style={{ width: "35px", height: "35px" }} />
+          </CloseButton>
+        </ModalHeader>
+        <ModalContent>{children}</ModalContent>
+      </ModalBody>
+    </Modal>,
+    document.getElementById("modal")
+  );
+}
+
+export default ModalPortal;
