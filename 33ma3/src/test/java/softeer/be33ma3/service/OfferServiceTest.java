f841374a53539d6597f66b2f283ec9015524e934
jbonnie <bonnie8623@gmail.com>
2024-02-18 19:08:46 +0900

    [TEST]: 견적 댓글 삭제 테스트
    - 성공적인 댓글 수정 테스트
    - 작성자가 아닌 다른 유저일 경우 테스트

--- 33ma3/src/test/java/softeer/be33ma3/service/OfferServiceTest.java
+++ 33ma3/src/test/java/softeer/be33ma3/service/OfferServiceTest.java
@@ -273,8 +273,54 @@ class OfferServiceTest {
         assertThat(exception.getMessage()).isEqualTo("기존 금액보다 낮은 금액으로만 수정 가능합니다.");
     }
 
+    @DisplayName("성공적으로 댓글을 삭제할 수 있다.")
     @Test
     void deleteOffer() {
+        // given
+        // post 저장
+        Post savedPost = createAndSavePost("승용차", "제네시스", 3, "서울시 강남구",
+                "기스, 깨짐", "오일 교체", new ArrayList<>(),"게시글 내용", null, null);
+        // center 저장
+        Member member = Member.createMember(2, "center1Id", "center1Pw");
+        Member savedMember = memberRepository.save(member);
+        Center center = Center.createCenter("center1", 0.0, 0.0, savedMember);
+        centerRepository.save(center);
+        // offer 저장
+        OfferCreateDto offerCreateDto = new OfferCreateDto(10, "offer1");
+        Long offerId = offerService.createOffer(savedPost.getPostId(), offerCreateDto, savedMember);
+        // when
+        offerService.deleteOffer(savedPost.getPostId(), offerId, savedMember);
+        // then
+        Optional<Offer> actual = offerRepository.findById(offerId);
+        assertThat(actual).isEmpty();
+    }
+
+    @DisplayName("댓글 작성자가 아닌 다른 유저의 댓글 삭제 요청시 예외가 발생한다.")
+    @Test
+    void deleteOfferWithNotWriter() {
+        // given
+        // post 저장
+        Post savedPost = createAndSavePost("승용차", "제네시스", 3, "서울시 강남구",
+                "기스, 깨짐", "오일 교체", new ArrayList<>(),"게시글 내용", null, null);
+        // center 저장
+        Member member = Member.createMember(2, "center1Id", "center1Pw");
+        Member savedMember = memberRepository.save(member);
+        Center center = Center.createCenter("center1", 0.0, 0.0, savedMember);
+        centerRepository.save(center);
+        // offer 저장
+        OfferCreateDto offerCreateDto = new OfferCreateDto(10, "offer1");
+        Long offerId = offerService.createOffer(savedPost.getPostId(), offerCreateDto, savedMember);
+        // 새로운 center 생성
+        Member member2 = Member.createMember(2, "center2Id", "center2Pw");
+        Member savedMember2 = memberRepository.save(member2);
+        Center center2 = Center.createCenter("center2", 0.0, 0.0, savedMember2);
+        centerRepository.save(center2);
+        // when
+        UnauthorizedException exception = assertThrows(UnauthorizedException.class, () -> {
+            offerService.deleteOffer(savedPost.getPostId(), offerId, savedMember2);
+        });
+        // then
+        assertThat(exception.getMessage()).isEqualTo("작성자만 삭제 가능합니다.");
     }
 
     @Test
