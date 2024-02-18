7b89e22ecb185c9edc32d81f8740cf1b43e47f92
Yang-soeun <soeun8636@naver.com>
2024-02-18 21:10:11 +0900

    [TEST]: ChatRoomRepository 테스트 코드 작성
    - 클라이언트와 센터 아이디로 채팅방 찾는 테스트 코드 작성

--- 33ma3/src/main/java/softeer/be33ma3/repository/ChatRoomRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/ChatRoomRepository.java
@@ -12,6 +12,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> findByCenter_MemberId(Long memberId);
 
     List<ChatRoom> findByClient_MemberId(Long memberId);
-    @Query(value = "SELECT cr.chatRoomId FROM ChatRoom cr WHERE cr.client.memberId = :centerId AND cr.center.memberId = :clientId")
+    @Query(value = "SELECT cr.chatRoomId FROM ChatRoom cr WHERE cr.client.memberId = :clientId AND cr.center.memberId = :centerId")
     Optional<Long> findRoomIdByCenterIdAndClientId(@Param("centerId") Long centerId, @Param("clientId") Long clientId);
 }
---
+++ 33ma3/src/test/java/softeer/be33ma3/repository/ChatRoomRepositoryTest.java
@@ -0,0 +1,44 @@
+package softeer.be33ma3.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import softeer.be33ma3.domain.ChatRoom;
+import softeer.be33ma3.domain.Member;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ChatRoomRepositoryTest {
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private ChatRoomRepository chatRoomRepository;
+
+    @AfterEach
+    void tearDown() {
+        chatRoomRepository.deleteAllInBatch();
+        chatRoomRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("센터아이디와 클라이언트 아이디로 두 사용자의 채팅방을 찾을 수 있다.")
+    @Test
+    void indRoomIdByCenterIdAndClientId(){
+        //given
+        Member client = new Member(1, "client1", "1234");
+        client = memberRepository.save(client);
+        Member center = new Member(2, "center1", "1234");
+        center = memberRepository.save(center);
+
+        ChatRoom chatRoom = ChatRoom.createChatRoom(client, center);
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+
+        //when
+        Long roomId = chatRoomRepository.findRoomIdByCenterIdAndClientId(center.getMemberId(), client.getMemberId()).get();
+
+        //then
+        Assertions.assertThat(roomId).isEqualTo(savedChatRoom.getChatRoomId());
+    }
+}
