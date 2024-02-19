8798f299bfad91341b0261f5e63b1ffdf7af3352
chjih <81521128+chjih@users.noreply.github.com>
2024-02-19 17:11:56 +0900

    [REFACTOR]: 사용하지 않는 import 요소 삭제

--- src/pages/PostPage/components/AuctionResult.jsx
+++ src/pages/PostPage/components/AuctionResult.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-import AuctionPrice from "../../../components/post/AuctionPrice";
-import Comment from "../../../components/post/Comment";
 import OptionType from "../../../components/post/OptionType";
 import OfferList from "./OfferList";
 
 function AuctionResult({ offerList }) {
-  console.log("result");
   return (
     <OptionType title={"경매 결과"}>
       <OfferList offerList={offerList} disabled />
