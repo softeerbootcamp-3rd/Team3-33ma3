0f7f790b96276024b4095013eebacf90b51e711d
Yang-soeun <soeun8636@naver.com>
2024-02-20 23:48:22 +0900

    [REFACTOR]: stringCommaParsing 분리
    - 다른 클래스에서 필요해서 재사용 하기 위해서 유틸 클래스를 생성하여 분리

--- 33ma3/src/main/java/softeer/be33ma3/service/PostService.java
+++ 33ma3/src/main/java/softeer/be33ma3/service/PostService.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static softeer.be33ma3.exception.ErrorCode.*;
 import static softeer.be33ma3.service.MemberService.CENTER_TYPE;
 import static softeer.be33ma3.service.MemberService.CLIENT_TYPE;
+import static softeer.be33ma3.utils.StringParser.stringCommaParsing;
 
 @Service
 @RequiredArgsConstructor
@@ -187,20 +188,9 @@ public class PostService {
         if (!post.getMember().getMemberId().equals(member.getMemberId())) {
             throw new BusinessException(AUTHOR_ONLY_ACCESS);
         }
-
         if (!offerRepository.findByPost_PostId(postId).isEmpty()) { //댓글이 있는 경우(경매 시작 후)
             throw new BusinessException(PRE_AUCTION_ONLY);
         }
-
         return post;
-    }
-
-    // 구분자 콤마로 문자열 파싱 후 각각의 토큰에서 공백 제거 후 리스트 반환
-    private List<String> stringCommaParsing(String inputString) {
-        if(inputString == null || inputString.isBlank())
-            return List.of();
-        return Arrays.stream(inputString.split(","))
-                .map(String::strip)
-                .toList();
     }
 }
