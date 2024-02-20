b304b1b846253630311c692af22b5e8976ee39a7
jbonnie <bonnie8623@gmail.com>
2024-02-20 21:04:30 +0900

    [FEAT]: 해당 센터의 평균 별점을 반환하는 jpa query method 구현

--- 33ma3/src/main/java/softeer/be33ma3/repository/ReviewRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/ReviewRepository.java
@@ -1,10 +1,14 @@
 package softeer.be33ma3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import softeer.be33ma3.domain.Review;
 
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Optional<Review> findByPost_PostId(Long postId);
+
+    @Query("SELECT AVG(r.score) FROM Review r WHERE r.center.memberId = :memberId")
+    Optional<Double> findAvgScoreByCenterId(Long memberId);
 }
