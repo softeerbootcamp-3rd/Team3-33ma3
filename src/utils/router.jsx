19caaf6943cc42fcee1b2060a6e538ee761a1772
chjih <81521128+chjih@users.noreply.github.com>
2024-02-20 15:24:29 +0900

    [FIX]: 비회원 접근 제어 시 로그인 페이지로 이동 수정

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -8,7 +8,6 @@ import CenterListPage from "../pages/CenterListPage";
 import CenterInfoPage from "../pages/CenterInfoPage";
 import {
   Navigate,
-  Outlet,
   createBrowserRouter,
   useRouteLoaderData,
 } from "react-router-dom";
@@ -84,7 +83,8 @@ function RequireAuth({ children }) {
   const { accessToken, memberType } = useRouteLoaderData("root");
 
   if (!accessToken || Number(memberType) === CENTER_TYPE) {
-    return <Navigate to={"/auth"} />;
+    alert("로그인이 필요한 페이지입니다.");
+    return <Navigate to={"/auth?mode=login"} />;
   }
   return children;
 }
