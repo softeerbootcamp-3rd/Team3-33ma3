9b4ef1c80a4ec65fcf28c788415395582ed71913
cmw9957 <cmw9957@gmail.com>
2024-02-21 22:50:29 +0900

    [FEAT]: 로그인 및 회원가입 index 페이지 추가

--- src/pages/AuthenticationPage/index.jsx
+++ src/pages/AuthenticationPage/index.jsx
@@ -1,3 +1,27 @@
-function AuthenticationPage() {}
+import { Navigate, useSearchParams } from "react-router-dom";
+import Page from "../../components/post/Page";
+import { Login } from "./components/Login";
+import { SignUp } from "./components/SignUp";
+import { removeAuthToken } from "../../utils/auth";
+
+function AuthenticationPage() {
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get("mode");
+  const isLogin = mode === "login";
+  const isSignUp = mode === "signUp";
+  const isLogOut = mode === "logout";
+
+  if (isLogOut) {
+    removeAuthToken();
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Page>
+      {isLogin && <Login />}
+      {isSignUp && <SignUp />}
+    </Page>
+  );
+}
 
 export { AuthenticationPage };
