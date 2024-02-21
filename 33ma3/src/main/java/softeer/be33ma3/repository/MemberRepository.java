27e834995b42375a00206ffbd757a0f733df5792
jbonnie <bonnie8623@gmail.com>
2024-02-21 13:39:24 +0900

    [FEAT]: 멤버 아이디를 이용하여 해당 멤버의 프로필 사진 링크를 반환하는 쿼리 메소드 구현

--- 33ma3/src/main/java/softeer/be33ma3/repository/MemberRepository.java
+++ 33ma3/src/main/java/softeer/be33ma3/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package softeer.be33ma3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import softeer.be33ma3.domain.Member;
 
 import java.util.Optional;
@@ -11,4 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByRefreshToken(String refreshToken);
 
     Optional<Member> findByLoginIdAndPassword(String loginId, String password);
+
+    @Query("SELECT m.image.link FROM Member m WHERE m.memberId = :memberId")
+    Optional<String> findProfileLinkByMemberId(Long memberId);
 }
