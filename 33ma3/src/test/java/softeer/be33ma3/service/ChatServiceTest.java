a9c237b700a8143c75b44380b6774ca71dc6b918
Yang-soeun <soeun8636@naver.com>
2024-02-24 20:03:41 +0900

    [TEST]: createRoom 서비스 테스트 코드 작성
    - 이미 방이 존재하는 경우 기존 방 아이디 반환 테스트 작성
    - 게시글 작성자가 아닌 다른 사용자가 방을 만드는 경우 예외 테스트 작성

--- 33ma3/src/test/java/softeer/be33ma3/service/ChatServiceTest.java
+++ 33ma3/src/test/java/softeer/be33ma3/service/ChatServiceTest.java
@@ -1,27 +1,28 @@
 package softeer.be33ma3.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import softeer.be33ma3.domain.ChatMessage;
 import softeer.be33ma3.domain.ChatRoom;
 import softeer.be33ma3.domain.Member;
 import softeer.be33ma3.domain.Post;
-import softeer.be33ma3.dto.request.ChatMessageDto;
 import softeer.be33ma3.dto.request.PostCreateDto;
+import softeer.be33ma3.exception.BusinessException;
 import softeer.be33ma3.repository.*;
 import softeer.be33ma3.repository.Chat.ChatMessageRepository;
 import softeer.be33ma3.repository.Chat.ChatRoomRepository;
 import softeer.be33ma3.repository.post.PostRepository;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static softeer.be33ma3.exception.ErrorCode.ONLY_POST_AUTHOR_ALLOWED;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -32,6 +33,13 @@ class ChatServiceTest {
     @Autowired PostRepository postRepository;
     @Autowired ChatMessageRepository chatMessageRepository;
 
+    @BeforeEach
+    void setUp(){
+        Member member1 = new Member(1, "client1", "1234", null);
+        Member member2 = new Member(1, "center1", "1234", null);
+        Member other = new Member(1, "other", "1234", null);
+        memberRepository.saveAll(List.of(member1, member2, other));
+    }
     @AfterEach
     void tearDown() {
         chatMessageRepository.deleteAllInBatch();
@@ -44,42 +52,50 @@ class ChatServiceTest {
     @Test
     void createRoom(){
         //given
-        Member client = new Member(1, "client1", "1234", null);
-        Member savedClient = memberRepository.save(client);
-        Member center = new Member(2, "center1", "1234", null);
-        Member savedCenter = memberRepository.save(center);
+        Member client = memberRepository.findMemberByLoginId("client1").get();
+        Member center = memberRepository.findMemberByLoginId("center1").get();
 
-        Long postId = createPost(savedClient).getPostId();
+        Long postId = createPost(client).getPostId();
 
         //when
-        Long roomId = chatService.createRoom(savedClient, savedCenter.getMemberId(), postId);
+        Long roomId = chatService.createRoom(client, center.getMemberId(), postId);
 
         //then
         assertThat(roomId).isNotNull();
     }
 
-    @DisplayName("채팅방에서 메세지를 전송할 수 있다.")
+    @DisplayName("게시글 작성자가 아닌 다른 사람이 채팅방을 만들면 예외가 발생한다.")
     @Test
-    void sendMessage() throws IOException {
+    void creatRoomOtherMember(){
         //given
-        Member client = new Member(1, "client1", "1234", null);
-        Member savedClient = memberRepository.save(client);
-        Member center = new Member(2, "center1", "1234", null);
-        Member savedCenter = memberRepository.save(center);
+        Member client = memberRepository.findMemberByLoginId("client1").get();
+        Member other = memberRepository.findMemberByLoginId("other").get();
+        Member center = memberRepository.findMemberByLoginId("center1").get();
 
-        ChatRoom chatRoom = ChatRoom.createChatRoom(savedClient, savedCenter);
-        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+        Long postId = createPost(client).getPostId();
 
-        ChatMessageDto chatMessageDto = new ChatMessageDto(chatRoom.getChatRoomId(), client.getMemberId(), center.getMemberId(), "내용");
+        //when //then
+        assertThatThrownBy(() -> chatService.createRoom(other, center.getMemberId(), postId))
+                .isInstanceOf(BusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ONLY_POST_AUTHOR_ALLOWED);
+    }
+
+    @DisplayName("이미 같은 일반 사용자와 센터의 방이 존재하는 경우 기존 아이디를 반환한다.")
+    @Test
+    void createRoomExist(){
+        //given
+        Member client = memberRepository.findMemberByLoginId("client1").get();
+        Member center = memberRepository.findMemberByLoginId("center1").get();
+
+        ChatRoom chatRoom = ChatRoom.createChatRoom(client, center);
+        Long existChatRoomId = chatRoomRepository.save(chatRoom).getChatRoomId();
+        Long postId = createPost(client).getPostId();
 
         //when
-        chatService.sendChatMessage(chatMessageDto);
+        Long roomId = chatService.createRoom(client, center.getMemberId(), postId);
 
         //then
-        List<ChatMessage> chatMessages = chatMessageRepository.findByChatRoom_ChatRoomId(savedChatRoom.getChatRoomId());
-        assertThat(chatMessages).hasSize(1)
-                .extracting("contents")
-                .containsExactly("내용");
+        assertThat(roomId).isEqualTo(existChatRoomId);
     }
 
     private Post createPost(Member savedClient) {
