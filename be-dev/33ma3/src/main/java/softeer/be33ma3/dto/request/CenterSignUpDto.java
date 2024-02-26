979808742edd069cb10c93b76b639a20b577a7b0
Yang-soeun <soeun8636@naver.com>
2024-02-27 03:17:47 +0900

    [TEST]: 회원가입 컨트롤러 테스트 코드 작성
    - 일반 사용자 회원가입 테스트 코드 작성
    - 센터 회원가입 테스트 코드 작성
    - 이미지 없이 회원가입 하는 경우 테스트 코드 작성

--- be-dev/33ma3/src/main/java/softeer/be33ma3/dto/request/CenterSignUpDto.java
+++ be-dev/33ma3/src/main/java/softeer/be33ma3/dto/request/CenterSignUpDto.java
@@ -3,9 +3,11 @@ package softeer.be33ma3.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Schema(description = "센터 회원가입 요청 DTO")
+@NoArgsConstructor
 public class CenterSignUpDto extends ClientSignUpDto {
 
     @Schema(description = "위도", example = "37.1234")
--- be-dev/33ma3/src/test/java/softeer/be33ma3/controller/MemberControllerTest.java
+++ be-dev/33ma3/src/test/java/softeer/be33ma3/controller/MemberControllerTest.java
@@ -8,12 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import softeer.be33ma3.dto.request.CenterSignUpDto;
+import softeer.be33ma3.dto.request.ClientSignUpDto;
 import softeer.be33ma3.dto.request.LoginDto;
 import softeer.be33ma3.jwt.JwtService;
 import softeer.be33ma3.service.MemberService;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,6 +35,59 @@ class MemberControllerTest {
     @Autowired private ObjectMapper objectMapper;
     @MockBean private MemberService memberService;
     @MockBean private JwtService jwtService;
+
+    @DisplayName("일반 사용자 회원가입")
+    @Test
+    void clientSignUp() throws Exception {
+        //given
+        ClientSignUpDto clientSignUpDto = new ClientSignUpDto("test", "1234");
+        String request = objectMapper.writeValueAsString(clientSignUpDto);
+        MockMultipartFile profile = createImages();
+
+        //when //then
+        mockMvc.perform(multipart("/client/signUp")
+                        .file(new MockMultipartFile("request", "", "application/json", request.getBytes(StandardCharsets.UTF_8)))
+                        .file(profile)
+                        .contentType(MULTIPART_FORM_DATA)
+                        .accept(APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.message").value("회원가입 성공"));
+    }
+
+    @DisplayName("이미지가 없어도 회원가입을 할 수 있다.")
+    @Test
+    void clientSignUpWithOutImage() throws Exception {
+        //given
+        ClientSignUpDto clientSignUpDto = new ClientSignUpDto("test", "1234");
+        String request = objectMapper.writeValueAsString(clientSignUpDto);
+
+        //when //then
+        mockMvc.perform(multipart("/client/signUp")
+                        .file(new MockMultipartFile("request", "", "application/json", request.getBytes(StandardCharsets.UTF_8)))
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.message").value("회원가입 성공"));
+    }
+
+    @DisplayName("센터 회원가입")
+    @Test
+    void centerSignUp() throws Exception {
+        //given
+        CenterSignUpDto centerSignUpDto = new CenterSignUpDto("test", "1234", 37.5, 127.0);
+        String request = objectMapper.writeValueAsString(centerSignUpDto);
+
+        //when //then
+        mockMvc.perform(multipart("/center/signUp")
+                        .file(new MockMultipartFile("request", "", "application/json", request.getBytes(StandardCharsets.UTF_8)))
+                        .accept(APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.message").value("회원가입 성공"));
+    }
 
     @DisplayName("센터, 일반 사용자 로그인 기능")
     @Test
@@ -41,5 +103,20 @@ class MemberControllerTest {
                 .andExpect(jsonPath("$.status").value("SUCCESS"))
                 .andExpect(jsonPath("$.message").value("로그인 성공"));
 
+    }
+
+    private MockMultipartFile createImages() throws IOException {
+        String fileName = "testImage"; //파일명
+        String contentType = "jpg"; //파일타입
+        String filePath = "src/test/resources/testImage/"+fileName+"."+contentType;
+
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "images",
+                fileName + "." + contentType,
+                contentType,
+                fileInputStream
+        );
+        return multipartFile;
     }
 }
