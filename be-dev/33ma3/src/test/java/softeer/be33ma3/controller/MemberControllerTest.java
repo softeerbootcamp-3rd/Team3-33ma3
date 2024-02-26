815c8544b3a5c895cf4ad4a755ef9ab9b5dd4281
Yang-soeun <soeun8636@naver.com>
2024-02-27 03:39:26 +0900

    [TEST]: 리프레시 토큰으로 재발급 받는 메소드 테스트 코드 작성

--- be-dev/33ma3/src/test/java/softeer/be33ma3/controller/MemberControllerTest.java
+++ be-dev/33ma3/src/test/java/softeer/be33ma3/controller/MemberControllerTest.java
@@ -2,6 +2,8 @@ package softeer.be33ma3.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +11,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import softeer.be33ma3.domain.Member;
 import softeer.be33ma3.dto.request.CenterSignUpDto;
 import softeer.be33ma3.dto.request.ClientSignUpDto;
 import softeer.be33ma3.dto.request.LoginDto;
+import softeer.be33ma3.jwt.JwtProvider;
 import softeer.be33ma3.jwt.JwtService;
+import softeer.be33ma3.jwt.JwtToken;
+import softeer.be33ma3.repository.MemberRepository;
 import softeer.be33ma3.service.MemberService;
 
 import java.io.FileInputStream;
@@ -29,12 +37,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 class MemberControllerTest {
+    private static String refreshToken;
+    private static String accessToken;
 
     @Autowired private MockMvc mockMvc;
     @Autowired private ObjectMapper objectMapper;
     @MockBean private MemberService memberService;
     @MockBean private JwtService jwtService;
+    @Autowired private JwtProvider jwtProvider;
+    @Autowired private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp(){
+        Member member = Member.createClient("테스트", "1234", null);
+        Member savedMember = memberRepository.save(member);
+        JwtToken jwtToken = jwtProvider.createJwtToken(savedMember.getMemberType(), savedMember.getMemberId(), savedMember.getLoginId());
+
+        refreshToken = jwtToken.getRefreshToken();
+        accessToken = jwtToken.getAccessToken();
+        savedMember.setRefreshToken(refreshToken);
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.deleteAllInBatch();
+    }
 
     @DisplayName("일반 사용자 회원가입")
     @Test
@@ -120,6 +150,19 @@ class MemberControllerTest {
                 .andExpect(jsonPath("$.status").value("SUCCESS"))
                 .andExpect(jsonPath("$.message").value("로그인 성공"));
 
+    }
+
+    @DisplayName("리프레시 토큰으로 엑세스 토큰을 재발급 받을 수 있다.")
+    @Test
+    void reissueToken() throws Exception {
+        //given //when //then
+        mockMvc.perform(post("/reissueToken")
+                        .header("Authorization-refresh", refreshToken)
+                        .header("Authorization", accessToken)
+                        .contentType(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.message").value("토큰 재발급 성공"));
     }
 
     private MockMultipartFile createImages() throws IOException {
