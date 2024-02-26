9516f3c1fc675a4de802704905c74a2754f46dde
Yang-soeun <soeun8636@naver.com>
2024-02-26 21:37:12 +0900

    [TEST]: 회원가입 서비스 테스트 코드 작성
    - 일반 유저 회원가입 테스트 코드 작성
    - 센터 회원가입 테스트 코드 작성

--- be-dev/33ma3/src/test/java/softeer/be33ma3/service/MemberServiceTest.java
+++ be-dev/33ma3/src/test/java/softeer/be33ma3/service/MemberServiceTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import softeer.be33ma3.domain.Member;
+import softeer.be33ma3.dto.request.CenterSignUpDto;
+import softeer.be33ma3.dto.request.ClientSignUpDto;
 import softeer.be33ma3.dto.request.LoginDto;
 import softeer.be33ma3.exception.BusinessException;
 import softeer.be33ma3.exception.ErrorCode;
 import softeer.be33ma3.repository.CenterRepository;
 import softeer.be33ma3.repository.MemberRepository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -34,6 +40,40 @@ class MemberServiceTest {
     void tearDown(){
         centerRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("일반 사용자 회원가입")
+    @Test
+    void clientSignUp() throws IOException {
+        //given
+        MockMultipartFile profile = createImages();
+        ClientSignUpDto clientSignUpDto = new ClientSignUpDto("test1", "1234");
+
+        //when
+        memberService.clientSignUp(clientSignUpDto, profile);
+
+        //then
+        Member member = memberRepository.findByLoginIdAndPassword("test1", "1234").get();
+        assertThat(member)
+                .extracting("loginId", "password")
+                .containsExactly("test1", "1234");
+    }
+
+    @DisplayName("센터 회원가입")
+    @Test
+    void centerSignUp() throws IOException {
+        //given
+        MockMultipartFile profile = createImages();
+        CenterSignUpDto centerSignUpDto = new CenterSignUpDto("test1", "1234", 37.5, 127.0);
+
+        //when
+        memberService.clientSignUp(centerSignUpDto, profile);
+
+        //then
+        Member member = memberRepository.findByLoginIdAndPassword("test1", "1234").get();
+        assertThat(member)
+                .extracting("loginId", "password")
+                .containsExactly("test1", "1234");
     }
 
     @DisplayName("센터와 일반 사용자 로그인 기능")
@@ -60,5 +100,20 @@ class MemberServiceTest {
         assertThatThrownBy(() -> memberService.login(loginDto))
                 .isInstanceOf(BusinessException.class)
                 .hasFieldOrPropertyWithValue("errorCode", ErrorCode.ID_PASSWORD_MISMATCH);
+    }
+
+    private MockMultipartFile createImages() throws IOException {
+        String fileName = "testImage"; //파일명
+        String contentType = "jpg"; //파일타입
+        String filePath = "src/test/resources/testImage/"+fileName+"."+contentType;
+
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "images",
+                fileName + "." + contentType,
+                contentType,
+                fileInputStream
+        );
+        return multipartFile;
     }
 }
